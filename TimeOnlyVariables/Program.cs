8fcabbf5cd2cb7a25df2238a4ba40e5482731b91
mgapp <mgapp@MIKEGYO>
2025-06-03 18:12:24 -0400

    Add project files.

---
+++ TimeOnlyVariables/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("What time is it?");
+string? timeEntered = Console.ReadLine(); 
+bool isValidTime = TimeOnly.TryParse(timeEntered, out TimeOnly time);
+while (isValidTime == false)
+{
+    Console.WriteLine("Time is incorrect, please try again. Must enter in following format:");
+    Console.WriteLine("H:MM AM/PM e.g. '8:00 PM'");
+    timeEntered = Console.ReadLine();
+    isValidTime = TimeOnly.TryParse(timeEntered, out time);
+}
